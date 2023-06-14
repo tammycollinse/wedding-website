58c37059412f62180555b58571acbe0ec03ecf3a
Tammy <tammycollinse@gmail.com>
2023-06-14 09:40:10 -0400

    cara function

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("something goes here so that I can see that these two are connected")
+
+function caraThing(){
+    console.log("I am adding this here so that I can push something ton getup")
+    console.log("but this is going to be the carasoule function")
+}
